ccc59c9374762d4012c3d9301711647b30422fb8
Willian Fagundes <61697250+Willian-Fagundes@users.noreply.github.com>
2025-10-27 17:20:16 +0000

    added error message in case of invalid input

--- UNITCONVERTER/Converter/Program.cs
+++ UNITCONVERTER/Converter/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-					
+using System.ComponentModel;
+
 double temp = 0;
 double time = 0;
 double extent = 0;
@@ -10,6 +11,7 @@ while(isValid == true)
 {	
 	bool valid = true;
 	string selection = "";
+	bool validate = false;
 	
 	Console.WriteLine("Select the measure converter:");
 	Console.WriteLine("1. Temperature\n2. Time\n3. Lenght\n0. Exit");
@@ -43,13 +45,16 @@ while(isValid == true)
 				switch(input)
 				{
 					case "1":
+						
 						Console.Clear();
 						Console.WriteLine("Insert the celsius temperature:");
 						readResult = Console.ReadLine();
 						if (readResult != null)
 						{
-							temp = DoubleConvert(readResult);
-						}
+							temp = DoubleConvert(readResult);                                                          
+                            
+						}		
+					
 						Console.Clear();
 						Console.WriteLine("Select the measure to convert to:");
 						Console.WriteLine("1. Fahrenheit\n2. Kelvin");
@@ -61,12 +66,12 @@ while(isValid == true)
 						if (input == "1")
 						{
 							Console.Clear();
-							Console.WriteLine($"Temperature in Fahrenheit:\n{(temp * 1.8) + 32}");
+							Console.WriteLine($"Temperature in Fahrenheit:\n{(temp * 1.8) + 32:F2}");
 						}
 						else if (input == "2")
 						{
 							Console.Clear();
-							Console.WriteLine($"Temperature in Kelvin:\n{temp + 273.15}");
+							Console.WriteLine($"Temperature in Kelvin:\n{temp + 273.15:F2}");
 						}
 						Console.WriteLine("\nNew Convert?\n1. Yes\n2. No");
 						readResult = Console.ReadLine();	
@@ -95,9 +100,9 @@ while(isValid == true)
 						}
 						Console.Clear();
 						if(input == "1")
-							Console.WriteLine($"Temperature in Celsius:\n{(temp - 32) * 1.8}");
+							Console.WriteLine($"Temperature in Celsius:\n{(temp - 32) * 1.8:F2}");
 						else if(input == "2")
-							Console.WriteLine($"Temperature in Fahrenheit:\n{((temp - 32) * 1.8) + 273.15}");
+							Console.WriteLine($"Temperature in Fahrenheit:\n{((temp - 32) * 1.8) + 273.15:F2}");
 						Console.WriteLine("\nNew Convert?\n1. Yes\n2. No");
 						readResult = Console.ReadLine();	
 						if(readResult != null)
@@ -125,9 +130,9 @@ while(isValid == true)
 						}
 						Console.Clear();
 						if (input == "1")
-							Console.WriteLine($"Temperature in Celsius:\n{temp - 273.15}");
+							Console.WriteLine($"Temperature in Celsius:\n{temp - 273.15:F2}");
 						else if (input == "2")
-							Console.WriteLine($"Temperature in Fahrenheit:\n{(1.8 * (temp - 273.15)) + 32}");
+							Console.WriteLine($"Temperature in Fahrenheit:\n{(1.8 * (temp - 273.15)) + 32:F2}");
 
 						
 						Console.WriteLine("\nNew Convert ?\n1. Yes\n2. No");
@@ -179,15 +184,15 @@ while(isValid == true)
 						}
 					Console.Clear();
 					if(input == "1")
-						Console.WriteLine($"Value Converted: {time / 60} Minutes");
+						Console.WriteLine($"Value Converted: {time / 60:F2} Minutes");
 					else if(input == "2")
-						Console.WriteLine($"Value Converted: {time / 3600} Hours");
+						Console.WriteLine($"Value Converted: {time / 3600:F2} Hours");
 					else if(input == "3")
-						Console.WriteLine($"Value Converted: {time / 86400} Days");
+						Console.WriteLine($"Value Converted: {time / 86400:F2} Days");
 					else if(input == "4")
-						Console.WriteLine($"Value Converted: {time / 604800} Months");
+						Console.WriteLine($"Value Converted: {time / 604800:F2} Months");
 					else if(input == "5")
-						Console.WriteLine($"Value Converted: {time / (86400 * 30)} Years");
+						Console.WriteLine($"Value Converted: {time / (86400 * 30):F2} Years");
 					break;
 					//minutes
 				case "2":
@@ -208,15 +213,15 @@ while(isValid == true)
 					}
 					Console.Clear();
 					if(input == "1")
-						Console.WriteLine($"Value Converted: {time * 60} Seconds");
+						Console.WriteLine($"Value Converted: {time * 60:F2} Seconds");
 					else if(input == "2")
-						Console.WriteLine($"Value Converted: {time / 60} Hours");
+						Console.WriteLine($"Value Converted: {time / 60:F2} Hours");
 					else if(input == "3")
-						Console.WriteLine($"Value Converted: {time / 1440} Days");
+						Console.WriteLine($"Value Converted: {time / 1440:F2} Days");
 					else if(input == "4")
-						Console.WriteLine($"Value Converted: {time / 10080} Weeks");
+						Console.WriteLine($"Value Converted: {time / 10080:F2} Weeks");
 					else if(input == "5")
-						Console.WriteLine($"Value Converted: {time / 43800} Months");
+						Console.WriteLine($"Value Converted: {time / 43800:F2} Months");
 
 					break;
 
@@ -238,15 +243,15 @@ while(isValid == true)
 					}
 					Console.Clear();
 					if(input == "1")
-						Console.WriteLine($"Value Converted: {time * 3600} Seconds");
+						Console.WriteLine($"Value Converted: {time * 3600:F2} Seconds");
 					else if(input == "2")
-						Console.WriteLine($"Value Converted: {time *60} Minutes");
+						Console.WriteLine($"Value Converted: {time *60:F2} Minutes");
 					else if(input == "3")
-						Console.WriteLine($"Value Converted: {time / 24} Days");
+						Console.WriteLine($"Value Converted: {time / 24:F2} Days");
 					else if(input == "4")
-						Console.WriteLine($"Value Converted: {time / 168} Weeks");
+						Console.WriteLine($"Value Converted: {time / 168:F2} Weeks");
 					else if(input == "5")
-						Console.WriteLine($"Value Converted: {time / 730} Months");
+						Console.WriteLine($"Value Converted: {time / 730:F2} Months");
 
 					break;
 				case "4":
@@ -267,15 +272,15 @@ while(isValid == true)
 					}
 					Console.Clear();
 					if(input == "1")
-						Console.WriteLine($"Value Converted: {time * 86400} Seconds");
+						Console.WriteLine($"Value Converted: {time * 86400:F2} Seconds");
 					else if(input == "2")
-						Console.WriteLine($"Value Converted: {time * 1440} Minutes");
+						Console.WriteLine($"Value Converted: {time * 1440:F2} Minutes");
 					else if(input == "3")
-						Console.WriteLine($"Value Converted: {time * 24} Hours");
+						Console.WriteLine($"Value Converted: {time * 24:F2} Hours");
 					else if(input == "4")
-						Console.WriteLine($"Value Converted: {time / 7} Weeks");
+						Console.WriteLine($"Value Converted: {time / 7:F2} Weeks");
 					else if(input == "5")
-						Console.WriteLine($"Value Converted: {time / 30.417} Months");
+						Console.WriteLine($"Value Converted: {time / 30.417:F2} Months");
 					
 					break;
 				case "5":
@@ -296,15 +301,15 @@ while(isValid == true)
 					}
 					Console.Clear();
 					if(input == "1")
-						Console.WriteLine($"Value Converted: {time * 604800} Seconds");
+						Console.WriteLine($"Value Converted: {time * 604800:F2} Seconds");
 					else if(input == "2")
-						Console.WriteLine($"Value Converted: {time * 10080} Minutes");
+						Console.WriteLine($"Value Converted: {time * 10080:F2} Minutes");
 					else if(input == "3")
-						Console.WriteLine($"Value Converted: {time * 168} Hours");
+						Console.WriteLine($"Value Converted: {time * 168:F2} Hours");
 					else if(input == "4")
-						Console.WriteLine($"Value Converted: {time * 7} Days");
+						Console.WriteLine($"Value Converted: {time * 7:F2} Days");
 					else if(input == "5")
-						Console.WriteLine($"Value Converted: {time / 4.34} Months");
+						Console.WriteLine($"Value Converted: {time / 4.34:F2} Months");
 					break;
 				case "6":
 					Console.Clear();
@@ -324,15 +329,15 @@ while(isValid == true)
 					}
 					Console.Clear();
 					if(input == "1")
-						Console.WriteLine($"Value Converted: {time * (86400 * 30)} Seconds");
+						Console.WriteLine($"Value Converted: {time * (86400 * 30):F2} Seconds");
 					else if(input == "2")
-						Console.WriteLine($"Value Converted: {time * 43800} Minutes");
+						Console.WriteLine($"Value Converted: {time * 43800:F2} Minutes");
 					else if(input == "3")
-						Console.WriteLine($"Value Converted: {time * 730} Hours");
+						Console.WriteLine($"Value Converted: {time * 730:F2} Hours");
 					else if(input == "4")
-						Console.WriteLine($"Value Converted: {time * 30.417} Days");
+						Console.WriteLine($"Value Converted: {time * 30.417:F2} Days");
 					else if(input == "5")
-						Console.WriteLine($"Value Converted: {time * 4.34} Weeks");
+						Console.WriteLine($"Value Converted: {time * 4.34:F2} Weeks");
 					break;
 			}
 			Console.WriteLine("1. New Measure\n2. Exit");
@@ -377,27 +382,27 @@ while(isValid == true)
 						Console.Clear();
 						if (input == "1")
 						{
-							Console.WriteLine($"Value Converted: {extent / 100} Meters");
+							Console.WriteLine($"Value Converted: {extent / 100:F2} Meters");
 						}
 						else if (input == "2")
 						{
-							Console.WriteLine($"Value Converted: {extent / 100000} Kilometers");
+							Console.WriteLine($"Value Converted: {extent / 100000:F2} Kilometers");
 						}
 						else if (input == "3")
 						{
-							Console.WriteLine($"Value Converted: {extent / 2.54} Inches");
+							Console.WriteLine($"Value Converted: {extent / 2.54:F2} Inches");
 						}
 						else if (input == "4")
 						{
-							Console.WriteLine($"Value Converted: {extent / 30.48} Feets");
+							Console.WriteLine($"Value Converted: {extent / 30.48:F2} Feets");
 						}
 						else if (input == "5")
 						{
-							Console.WriteLine($"Value Converted: {extent / 91.44} Yards");
+							Console.WriteLine($"Value Converted: {extent / 91.44:F2} Yards");
 						}
 						else if (input == "6")
 						{
-							Console.WriteLine($"Value Converted: {extent / 160900} Miles");
+							Console.WriteLine($"Value Converted: {extent / 160900:F2} Miles");
 						}
 
 						Console.WriteLine("\nNew Conversion:\n1. Yes\n2. Exit");
@@ -407,9 +412,9 @@ while(isValid == true)
 							input = readResult;
 						}
 						if (input == "2")
-                        {
+						{
 							valid = false;
-                        }
+						}
 						break;
 					case "2":
 						Console.Clear();
@@ -430,27 +435,27 @@ while(isValid == true)
 						Console.Clear();
 						if (input == "1")
 						{
-							Console.WriteLine($"Value Converted: {extent * 100} Centimeters");
+							Console.WriteLine($"Value Converted: {extent * 100:F2} Centimeters");
 						}
 						else if (input == "2")
 						{
-							Console.WriteLine($"Value Converted: {extent / 1000} Kilometers");
+							Console.WriteLine($"Value Converted: {extent / 1000:F2} Kilometers");
 						}
 						else if (input == "3")
 						{
-							Console.WriteLine($"Value Converted: {extent * 39.37} Inches");
+							Console.WriteLine($"Value Converted: {extent * 39.37:F2} Inches");
 						}
 						else if (input == "4")
 						{
-							Console.WriteLine($"Value Converted: {extent * 3.281} Feets");
+							Console.WriteLine($"Value Converted: {extent * 3.281:F2} Feets");
 						}
 						else if (input == "5")
 						{
-							Console.WriteLine($"Value Converted: {extent / 1.094} Yards");
+							Console.WriteLine($"Value Converted: {extent / 1.094:F2} Yards");
 						}
 						else if (input == "6")
 						{
-							Console.WriteLine($"Value Converted: {extent / 1609} Miles");
+							Console.WriteLine($"Value Converted: {extent / 1609:F2} Miles");
 						}
 
 						Console.WriteLine("\nNew Conversion:\n1. Yes\n2. Exit");
@@ -460,9 +465,9 @@ while(isValid == true)
 							input = readResult;
 						}
 						if (input == "2")
-                        {
+						{
 							valid = false;
-                        }
+						}
 						break;
 					case "3":
 						Console.Clear();
@@ -483,27 +488,27 @@ while(isValid == true)
 						Console.Clear();
 						if (input == "1")
 						{
-							Console.WriteLine($"Value Converted: {extent * 100000} Centimeters");
+							Console.WriteLine($"Value Converted: {extent * 100000:F2} Centimeters");
 						}
 						else if (input == "2")
 						{
-							Console.WriteLine($"Value Converted: {extent * 1000} Meters");
+							Console.WriteLine($"Value Converted: {extent * 1000:F2} Meters");
 						}
 						else if (input == "3")
 						{
-							Console.WriteLine($"Value Converted: {extent * 39370} Inches");
+							Console.WriteLine($"Value Converted: {extent * 39370:F2} Inches");
 						}
 						else if (input == "4")
 						{
-							Console.WriteLine($"Value Converted: {extent * 3281} Feets");
+							Console.WriteLine($"Value Converted: {extent * 3281:F2} Feets");
 						}
 						else if (input == "5")
 						{
-							Console.WriteLine($"Value Converted: {extent * 1094} Yards");
+							Console.WriteLine($"Value Converted: {extent * 1094:F2} Yards");
 						}
 						else if (input == "6")
 						{
-							Console.WriteLine($"Value Converted: {extent / 1.609} Miles");
+							Console.WriteLine($"Value Converted: {extent / 1.609:F2} Miles");
 						}
 
 						Console.WriteLine("\nNew Conversion:\n1. Yes\n2. Exit");
@@ -513,9 +518,9 @@ while(isValid == true)
 							input = readResult;
 						}
 						if (input == "2")
-                        {
+						{
 							valid = false;
-                        }
+						}
 						break;
 					case "4":
 						Console.Clear();
@@ -536,27 +541,27 @@ while(isValid == true)
 						Console.Clear();
 						if (input == "1")
 						{
-							Console.WriteLine($"Value Converted: {extent * 2.54} Centimeters");
+							Console.WriteLine($"Value Converted: {extent * 2.54:F2} Centimeters");
 						}
 						else if (input == "2")
 						{
-							Console.WriteLine($"Value Converted: {extent / 39.37} Meters");
+							Console.WriteLine($"Value Converted: {extent / 39.37:F2} Meters");
 						}
 						else if (input == "3")
 						{
-							Console.WriteLine($"Value Converted: {extent / 39370} Kilometers");
+							Console.WriteLine($"Value Converted: {extent / 39370:F2} Kilometers");
 						}
 						else if (input == "4")
 						{
-							Console.WriteLine($"Value Converted: {extent / 12} Feets");
+							Console.WriteLine($"Value Converted: {extent / 12:F2} Feets");
 						}
 						else if (input == "5")
 						{
-							Console.WriteLine($"Value Converted: {extent / 36} Yards");
+							Console.WriteLine($"Value Converted: {extent / 36:F2} Yards");
 						}
 						else if (input == "6")
 						{
-							Console.WriteLine($"Value Converted: {extent / 63360} Miles");
+							Console.WriteLine($"Value Converted: {extent / 63360:F2} Miles");
 						}
 
 						Console.WriteLine("\nNew Conversion:\n1. Yes\n2. Exit");
@@ -566,9 +571,9 @@ while(isValid == true)
 							input = readResult;
 						}
 						if (input == "2")
-                        {
+						{
 							valid = false;
-                        }
+						}
 						break;
 					case "5":
 						Console.Clear();
@@ -589,27 +594,27 @@ while(isValid == true)
 						Console.Clear();
 						if (input == "1")
 						{
-							Console.WriteLine($"Value Converted: {extent * 30.48} Centimeters");
+							Console.WriteLine($"Value Converted: {extent * 30.48:F2} Centimeters");
 						}
 						else if (input == "2")
 						{
-							Console.WriteLine($"Value Converted: {extent / 3.281} Meters");
+							Console.WriteLine($"Value Converted: {extent / 3.281:F2} Meters");
 						}
 						else if (input == "3")
 						{
-							Console.WriteLine($"Value Converted: {extent / 3281} Kilometers");
+							Console.WriteLine($"Value Converted: {extent / 3281:F2} Kilometers");
 						}
 						else if (input == "4")
 						{
-							Console.WriteLine($"Value Converted: {extent * 12} Inches");
+							Console.WriteLine($"Value Converted: {extent * 12:F2} Inches");
 						}
 						else if (input == "5")
 						{
-							Console.WriteLine($"Value Converted: {extent / 3} Yards");
+							Console.WriteLine($"Value Converted: {extent / 3:F2} Yards");
 						}
 						else if (input == "6")
 						{
-							Console.WriteLine($"Value Converted: {extent / 5280} Miles");
+							Console.WriteLine($"Value Converted: {extent / 5280:F2} Miles");
 						}
 
 						Console.WriteLine("\nNew Conversion:\n1. Yes\n2. Exit");
@@ -619,9 +624,9 @@ while(isValid == true)
 							input = readResult;
 						}
 						if (input == "2")
-                        {
+						{
 							valid = false;
-                        }
+						}
 						break;
 					case "6":
 						Console.Clear();
@@ -642,27 +647,27 @@ while(isValid == true)
 						Console.Clear();
 						if (input == "1")
 						{
-							Console.WriteLine($"Value Converted: {extent * 91.84} Centimeters");
+							Console.WriteLine($"Value Converted: {extent * 91.84:F2} Centimeters");
 						}
 						else if (input == "2")
 						{
-							Console.WriteLine($"Value Converted: {extent / 1.094} Meters");
+							Console.WriteLine($"Value Converted: {extent / 1.094:F2} Meters");
 						}
 						else if (input == "3")
 						{
-							Console.WriteLine($"Value Converted: {extent / 1094} Kilometers");
+							Console.WriteLine($"Value Converted: {extent / 1094:F2} Kilometers");
 						}
 						else if (input == "4")
 						{
-							Console.WriteLine($"Value Converted: {extent * 36} Inches");
+							Console.WriteLine($"Value Converted: {extent * 36:F2} Inches");
 						}
 						else if (input == "5")
 						{
-							Console.WriteLine($"Value Converted: {extent * 3} Feets");
+							Console.WriteLine($"Value Converted: {extent * 3:F2} Feets");
 						}
 						else if (input == "6")
 						{
-							Console.WriteLine($"Value Converted: {extent / 1760} Miles");
+							Console.WriteLine($"Value Converted: {extent / 1760:F2} Miles");
 						}
 
 						Console.WriteLine("\nNew Conversion:\n1. Yes\n2. Exit");
@@ -672,9 +677,9 @@ while(isValid == true)
 							input = readResult;
 						}
 						if (input == "2")
-                        {
+						{
 							valid = false;
-                        }
+						}
 						break;
 					case "7":
 						Console.Clear();
@@ -695,27 +700,27 @@ while(isValid == true)
 						Console.Clear();
 						if (input == "1")
 						{
-							Console.WriteLine($"Value Converted: {extent * 160900} Centimeters");
+							Console.WriteLine($"Value Converted: {extent * 160900:F2} Centimeters");
 						}
 						else if (input == "2")
 						{
-							Console.WriteLine($"Value Converted: {extent * 1609} Meters");
+							Console.WriteLine($"Value Converted: {extent * 1609:F2} Meters");
 						}
 						else if (input == "3")
 						{
-							Console.WriteLine($"Value Converted: {extent * 1.609} Kilometers");
+							Console.WriteLine($"Value Converted: {extent * 1.609:F2} Kilometers");
 						}
 						else if (input == "4")
 						{
-							Console.WriteLine($"Value Converted: {extent * 63360} Inches");
+							Console.WriteLine($"Value Converted: {extent * 63360:F2} Inches");
 						}
 						else if (input == "5")
 						{
-							Console.WriteLine($"Value Converted: {extent * 5280} Feets");
+							Console.WriteLine($"Value Converted: {extent * 5280:F2} Feets");
 						}
 						else if (input == "6")
 						{
-							Console.WriteLine($"Value Converted: {extent * 1760} Yards");
+							Console.WriteLine($"Value Converted: {extent * 1760:F2} Yards");
 						}
 
 						Console.WriteLine("\nNew Conversion:\n1. Yes\n2. Exit");
@@ -725,11 +730,11 @@ while(isValid == true)
 							input = readResult;
 						}
 						if (input == "2")
-                        {
+						{
 							valid = false;
-                        }
+						}
 						break;
-                }
+				}
 			} while (valid == true);
 			break;
 
@@ -739,7 +744,20 @@ while(isValid == true)
 // Metodo de conversão
 double DoubleConvert(string word)
 {
-	double val = 0;
-	bool isValid = double.TryParse(word, out val);
-	return val;
+	double resultado;
+	try
+	{
+		resultado = double.Parse(word);
+
+        return Math.Round(resultado, 2);
+	}
+
+	catch (FormatException ex)
+	{
+		throw new ArgumentException($"Valor '{word}' não é um número double válido.", nameof(word), ex);
+	}
+	catch (OverflowException ex)
+	{
+		throw new ArgumentException($"Valor '{word}' é muito grande ou pequeno para um double.", nameof(word), ex);
+	}
 }
--- UNITCONVERTER/Converter/obj/Debug/net8.0/Converter.AssemblyInfo.cs
+++ UNITCONVERTER/Converter/obj/Debug/net8.0/Converter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Converter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cb26b17d29e255582f3f4bd76bcc55ec84f2269")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2805b3d8499ddc0960c330518134c65679e8de8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Converter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Converter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
